b2d9f913dda217776f56b08d023a9f6388426110
AlexCuellar <alexcuel@mit.edu>
2026-04-10 22:21:26 -0500

    Add publications

--- js/content.js
+++ js/content.js
@@ -7,7 +7,7 @@ window.SITE_CONTENT = {
   profileImage: "assets/Profile_Picture.jpg",
 
   /** Shown under the photo. */
-  name: "Your Name",
+  name: "Alex Cuellar",
 
   /**
    * Email shown as obfuscated text (bots). Change the display string and optional mailto.
@@ -29,7 +29,7 @@ window.SITE_CONTENT = {
    * Bio paragraphs (HTML allowed for inline links). Replace with your story.
    */
   bioHtml: `
-    <p>I am a PhD student in MIT’s AeroAstro Department advised by Prof. Julie Shah with the <a href="https://interactive.mit.edu/">Interactive Robotics Group</a>. My research interests lie in enabling robots to act and react efficiently to humans in close-proximity interaction. I am currently exploring methods to leverage structure in repetitive tasks to allow robots to learn and adapt to individuals’ tenancies, preferences, and abilities online.</p>
+    <p>I am a PhD asdfasdfstudent in MIT’s AeroAstro Department advised by Prof. Julie Shah with the <a href="https://interactive.mit.edu/">Interactive Robotics Group</a>. My research interests lie in enabling robots to act and react efficiently to humans in close-proximity interaction. I am currently exploring methods to leverage structure in repetitive tasks to allow robots to learn and adapt to individuals’ tenancies, preferences, and abilities online.</p>
     <p>I completed my Master’s of Engineering with the MIT EECS department under Prof. Julie Shah (2022) and competed my BS with the MIT EECS department (2021). Outside of research, I enjoy hiking, cooking, and acting with local theater groups.</p>
   `,
 
